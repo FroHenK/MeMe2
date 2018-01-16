e0b89506f289d1cb018edab0e36e18a1e2f71605
Askhat Khayrullin <harmony.moagic@ya.ru>
2018-01-16 09:58:37 +0300

    Login and Register

--- app/src/main/java/org/whysosirius/meme/MainActivity.java
+++ app/src/main/java/org/whysosirius/meme/MainActivity.java
@@ -20,10 +20,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import org.whysosirius.meme.database.Meme;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     final static String APP_PREFERENCES = "prefs";
@@ -59,16 +66,27 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
-        Intent login_intent = new Intent(this, LoginActivity.class);
-        startActivityForResult(login_intent, 1337);
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivityForResult(intent, 1337);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 1337){
             Log.i("Login", "Success");
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                // ...
+            } else {
+                // Sign in failed, check response for error code
+                // ...
+            }
         }
-            super.onActivityResult(requestCode, resultCode, data);
+
     }
 
     public static class PlaceholderFragment extends Fragment {
